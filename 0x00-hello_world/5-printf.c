995d8ae5e93e07ff445c10460d9587ee23529bf8
Chris-Emma <chrisemmaofficial@gmail.com>
2023-07-13 10:39:41 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always 0 when successfully compiled
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
