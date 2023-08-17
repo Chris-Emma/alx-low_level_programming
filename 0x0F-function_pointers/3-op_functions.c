3f3c60f04e154f8b8faf80644f6c682ded8a04d2
Chris-Emma <chrisemmaofficial@gmail.com>
2023-08-17 05:10:03 -0700

    simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: diff
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: prod
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: division result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates modulus
+ * @a: number 1
+ * @b: number 2
+ * Return: modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
