8c868530dd921a4633695e297a47b833193e394a
Chris-Emma <chrisemmaofficial@gmail.com>
2023-10-11 05:57:56 -0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+*print_name - prints name
+*@name:name tobe printed
+*@f: print name function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
