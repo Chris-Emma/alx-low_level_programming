aaa5680951c2a56ad8a01baf5098da25285242e6
Chris-Emma <chrisemmaofficial@gmail.com>
2023-10-24 01:16:12 -0700

    prints lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t list_count = 0;
-	const listint_t *curr_list = h;
 
-	while (curr_list != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", curr_list->n);
-		curr_list = curr_list->next;
-		list_count++;
+		printf("%d\n", h->n);
+		h = h->next;
+		list_count += 1;
 	}
 	return (list_count);
 }
