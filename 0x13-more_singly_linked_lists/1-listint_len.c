021dec753b5cbcc5d23880c746b54e0ea3393f8e
Chris-Emma <chrisemmaofficial@gmail.com>
2023-10-24 01:37:10 -0700

    list len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - finds number of elements in a linked list
+ * @h: head of a linked list
+ * Return: number of elements in a linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t list_len = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		list_len += 1;
+		h = h->next;
+	}
+	return (list_len);
+}
